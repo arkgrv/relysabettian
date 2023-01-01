1933188479a92d702dfd8957558c258438113511
arkgrv <ag.novitskiy@icloud.com>
2023-01-01 16:13:01 +0100

    Completed implementation of Common types for Compiler and Parser

---
+++ compiler/src/common.rs
@@ -0,0 +1,74 @@
+use std::rc::Rc;
+
+/// Describes the different modes of parsing
+/// precedence.
+#[derive(Copy, Clone)]
+pub enum Precedence {
+    None,
+    Assignment,
+    Or,
+    And,
+    Equality,
+    Comparison,
+    Term,
+    Factor,
+    Unary,
+    Call,
+    Primary,
+}
+
+/// Parsing function type
+pub type ParseFn = fn(bool);
+
+/// Implementation of a basic parsing rule
+#[derive(Clone)]
+pub struct ParseRule {
+    pub prefix: Rc<ParseFn>,
+    pub infix: Rc<ParseFn>,
+    pub precedence: Precedence,
+}
+
+/// Implementation of a local variable reference
+#[derive(Clone)]
+pub struct Local {
+    pub name: String,
+    pub depth: i32,
+    pub is_captured: bool,
+}
+
+impl Local {
+    /// Constructs a new Local value
+    /// 
+    /// Parameters:
+    /// * `name`: name as string
+    /// * `depth`: scope depth of the value
+    pub fn new(name: String, depth: i32) -> Local {
+        Local {
+            name: name.clone(),
+            depth,
+            is_captured: false,
+        }
+    }
+}
+
+/// Implementation of an internal compiler and parser
+/// upvalue type
+#[derive(Clone)]
+pub struct InternalUpvalue {
+    pub index: u8,
+    pub is_local: bool,
+}
+
+impl InternalUpvalue {
+    /// Constructs a new internal upvalue
+    /// 
+    /// Parameters:
+    /// * `index`: memory location of the upvalue
+    /// * `is_local`: locality of the upvalue
+    pub fn new(index: u8, is_local: bool) -> InternalUpvalue {
+        InternalUpvalue {
+            index,
+            is_local,
+        }
+    }
+}
--- compiler/src/lib.rs
+++ compiler/src/lib.rs
@@ -1,2 +1,5 @@
 pub mod value;
 pub mod instruction;
+pub mod common;
+pub mod compiler;
+pub mod parser;
