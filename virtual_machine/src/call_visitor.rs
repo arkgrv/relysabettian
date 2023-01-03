9ef34fe3dcad39e4b20a99b14a29acfb825839a3
arkgrv <ag.novitskiy@icloud.com>
2023-01-03 05:17:32 +0100

    Fixed CallVisitor

--- virtual_machine/src/call_visitor.rs
+++ virtual_machine/src/call_visitor.rs
@@ -1,51 +1,49 @@
-use std::{cell::RefCell, ops::Deref, rc::Rc};
-
 use compiler::value::{Closure, NativeFunction, Value};
 
 use crate::{common::STACK_MAX, vm::VirtualMachine};
 
 /// Call visitor implementation
-pub struct CallVisitor<'a> {
+pub struct CallVisitor {
     pub arg_count: usize,
-    pub vm: Rc<RefCell<&'a mut VirtualMachine>>,
+    pub vm: *mut VirtualMachine,
 }
 
-impl CallVisitor<'_> {
+impl CallVisitor {
     /// Constructs a new Call visitor
     ///
     /// Parameters:
     /// * `arg_count`: number of arguments
     /// * `vm`: virtual machine
-    pub fn new(arg_count: usize, vm: Rc<RefCell<&mut VirtualMachine>>) -> CallVisitor {
+    pub fn new(arg_count: usize, vm: *mut VirtualMachine) -> CallVisitor {
         CallVisitor {
             arg_count,
-            vm: Rc::clone(&vm),
+            vm,
         }
     }
 
-    pub fn visit(&mut self, value: Rc<RefCell<Value>>) -> bool {
-        match (*value).borrow().deref() {
-            Value::NativeFunction(nf) => self.visit_native_fn(Rc::clone(&nf)),
-            Value::Closure(cl) => self.visit_closure(Rc::clone(&cl)),
+    pub fn visit(&mut self, value: *mut Value) -> bool {
+        match unsafe { value.as_ref().unwrap() } {
+            Value::NativeFunction(nf) => self.visit_native_fn(*nf),
+            Value::Closure(cl) => self.visit_closure(*cl),
             _ => false,
         }
     }
 
-    fn visit_native_fn(&mut self, native: Rc<NativeFunction>) -> bool {
-        let last = (*self.vm).borrow().frames.len();
-        let new_size = (*self.vm).borrow().frames.len() - self.arg_count - 1;
+    fn visit_native_fn(&mut self, native: *mut NativeFunction) -> bool {
+        let last = unsafe { self.vm.as_ref() }.unwrap().frames.len() - 1;
+        let new_size = unsafe { self.vm.as_ref() }.unwrap().frames.len() - self.arg_count - 1;
 
-        let args = &(*self.vm).borrow().stack[last - self.arg_count..=last];
-        let result = native.function.clone()(self.arg_count, args.to_vec());
+        let args = &unsafe { self.vm.as_ref() }.unwrap().stack[last - self.arg_count..=last];
+        let result = unsafe { *(*native).function }(self.arg_count, args.to_vec());
 
-        (*self.vm).borrow_mut().stack.resize(new_size, Value::Null);
-        (*self.vm).borrow_mut().stack.reserve(STACK_MAX);
-        (*self.vm).borrow_mut().push(result);
+        unsafe { self.vm.as_ref() }.unwrap().stack.resize(new_size, Value::Null);
+        unsafe { self.vm.as_ref() }.unwrap().stack.reserve(STACK_MAX);
+        unsafe { self.vm.as_ref() }.unwrap().push(result);
 
         true
     }
 
-    fn visit_closure(&mut self, closure: Rc<Closure>) -> bool {
-        (*self.vm).borrow_mut().call(closure, self.arg_count)
+    fn visit_closure(&mut self, closure: *mut Closure) -> bool {
+        unsafe { self.vm.as_ref() }.unwrap().call(closure, self.arg_count)
     }
 }
