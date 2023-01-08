971e4ed9202013361d7b6a0baac97f9fd8583743
arkgrv <ag.novitskiy@icloud.com>
2023-01-08 03:24:03 +0100

    Value now uses safe Rust

--- compiler/src/value.rs
+++ compiler/src/value.rs
@@ -1,9 +1,24 @@
-use std::{alloc::Layout, alloc::alloc, collections::HashMap};
+use std::{collections::HashMap, rc::Rc, cell::RefCell, ops::Deref};
 
 use crate::instruction::Opcode;
 
 /// Maximum number of UINT8 variables
 pub const UINT8_COUNT: u16 = u8::MAX as u16 + 1;
+
+/// Function type
+type Function = Rc<RefCell<FuncRepr>>;
+/// Native Function type
+type NativeFunction = Rc<RefCell<NativeFuncRepr>>;
+/// Closure type
+type Closure = Rc<RefCell<ClosureRepr>>;
+/// Upvalue type
+type Upvalue = Rc<RefCell<UpvalueRepr>>;
+/// Class type
+type Class = Rc<RefCell<ClassRepr>>;
+/// Instance type
+type Instance = Rc<RefCell<InstanceRepr>>;
+/// Method type
+type Method = Rc<RefCell<MethodRepr>>;
 
 /// Represents any valid value in the language's
 /// runtime environment.
@@ -13,13 +28,13 @@ pub enum Value {
     String(String),
     Double(f64),
     Bool(bool),
-    Function(*mut Function),
-    NativeFunction(*mut NativeFunction),
-    Closure(*mut Closure),
-    Upvalue(*mut Upvalue),
-    Class(*mut Class),
-    Instance(*mut Instance),
-    Method(*mut Method),
+    Function(Function),
+    NativeFunction(NativeFunction),
+    Closure(Closure),
+    Upvalue(Upvalue),
+    Class(Class),
+    Instance(Instance),
+    Method(Method),
 }
 
 /// This structure holds a chunk of memory which is
@@ -113,51 +128,51 @@ impl Chunk {
 }
 
 /// Native function type alias
-type NativeFn = fn(usize, Vec<Value>) -> Value;
+type NativeFn = Rc<RefCell<fn(usize, Vec<Value>) -> Value>>;
 
 /// Represents a native (runtime environment related)
 /// function.
 #[derive(Clone)]
-pub struct NativeFunction {
-    pub function: *mut NativeFn,
+pub struct NativeFuncRepr {
+    pub function: NativeFn,
 }
 
 /// Represents an upvalue in the language
 #[derive(Clone)]
-pub struct Upvalue {
-    pub location: *mut Value,
+pub struct UpvalueRepr {
+    pub location: Rc<RefCell<Value>>,
     pub closed: Value,
-    pub next: *mut Value,
+    pub next: Rc<RefCell<Option<Value>>>,
 }
 
-impl Upvalue {
+impl UpvalueRepr {
     /// Constructs a new Upvalue
     ///
     /// Parameters:
     /// * `slot`: pointer to the value's location
-    pub fn new(slot: *mut Value) -> Upvalue {
-        Upvalue {
-            location: slot,
+    pub fn new(slot: Rc<RefCell<Value>>) -> UpvalueRepr {
+        UpvalueRepr {
+            location: Rc::clone(&slot),
             closed: Value::Null,
-            next: std::ptr::null_mut(),
+            next: Rc::new(RefCell::new(None)),
         }
     }
 }
 
 /// Represents a class in the language
 #[derive(Clone)]
-pub struct Class {
+pub struct ClassRepr {
     pub name: String,
     pub methods: HashMap<String, Closure>,
 }
 
-impl Class {
+impl ClassRepr {
     /// Constructs a new Class
     ///
     /// Parameters:
     /// * `name`: name of the class
-    pub fn new(name: String) -> Class {
-        Class {
+    pub fn new(name: String) -> ClassRepr {
+        ClassRepr {
             name,
             methods: HashMap::new(),
         }
@@ -166,18 +181,18 @@ impl Class {
 
 /// Represents an instance of a class
 #[derive(Clone)]
-pub struct Instance {
-    pub class: *mut Class,
+pub struct InstanceRepr {
+    pub class: Class,
     pub fields: HashMap<String, Value>,
 }
 
-impl Instance {
+impl InstanceRepr {
     /// Constructs a new Instance
     ///
     /// Parameters:
     /// * `class`: instantiated class
-    pub fn new(class: *mut Class) -> Instance {
-        Instance {
+    pub fn new(class: Class) -> InstanceRepr {
+        InstanceRepr {
             class,
             fields: HashMap::new(),
         }
@@ -186,19 +201,19 @@ impl Instance {
 
 /// Represents a class' method
 #[derive(Clone)]
-pub struct Method {
-    pub receiver: *mut Instance,
-    pub method: *mut Closure,
+pub struct MethodRepr {
+    pub receiver: Instance,
+    pub method: Closure,
 }
 
-impl Method {
+impl MethodRepr {
     /// Constructs a new class method
     ///
     /// Parameters:
     /// * `receiver`: instance bound to this method
     /// * `method`: closure related to this method
-    pub fn new(receiver: *mut Instance, method: *mut Closure) -> Method {
-        Method {
+    pub fn new(receiver: Instance, method: Closure) -> MethodRepr {
+        MethodRepr {
             receiver,
             method,
         }
@@ -207,30 +222,30 @@ impl Method {
 
 /// Represents a function in the language
 #[derive(Clone)]
-pub struct Function {
+pub struct FuncRepr {
     pub arity: usize,
     pub upvalue_count: usize,
     pub name: String,
-    pub chunk: *mut Chunk,
+    pub chunk: Rc<RefCell<Chunk>>,
 }
 
-impl Function {
+impl FuncRepr {
     /// Constructs a new function
     ///
     /// Parameters:
     /// * `arity`: number of arguments
     /// * `name`: name of the function
-    pub fn new(arity: usize, name: String) -> Function {
-        Function {
+    pub fn new(arity: usize, name: String) -> FuncRepr {
+        FuncRepr {
             arity,
             upvalue_count: 0,
             name: name.clone(),
-            chunk: unsafe { alloc(Layout::new::<Chunk>()) as *mut Chunk },
+            chunk: Rc::new(RefCell::new(Chunk::new()))
         }
     }
 }
 
-impl PartialEq for Function {
+impl PartialEq for FuncRepr {
     fn eq(&self, _other: &Self) -> bool {
         false
     }
@@ -238,24 +253,24 @@ impl PartialEq for Function {
 
 /// Represents a function closure
 #[derive(Clone)]
-pub struct Closure {
-    pub function: *mut Function,
-    pub upvalues: Vec<*mut Upvalue>,
+pub struct ClosureRepr {
+    pub function: Function,
+    pub upvalues: Vec<Upvalue>,
 }
 
-impl Closure {
+impl ClosureRepr {
     /// Constructs a new Closure
     ///
     /// Parameters:
     /// * `function`: function bound to this closure
-    pub fn new(function: *mut Function) -> Closure {
-        let mut cl = Closure {
-            function: function.clone(),
+    pub fn new(function: Function) -> ClosureRepr {
+        let mut cl = ClosureRepr {
+            function: Rc::clone(&function),
             upvalues: Vec::new(),
         };
 
-        let uval_count = unsafe { (*function).upvalue_count };
-        cl.upvalues.resize(uval_count, std::ptr::null_mut());
+        let uval_count = function.deref().borrow().upvalue_count;
+        cl.upvalues.resize(uval_count, Rc::new(RefCell::new(UpvalueRepr::new(zero_init()))));
         cl
     }
 }
@@ -275,22 +290,22 @@ impl OutputVisitor for Value {
             Value::Bool(b) => print!("{}", if *b { "true" } else { "false" }),
             Value::String(s) => print!("{}", s),
             Value::Function(f) => {
-                if unsafe { (*(*f)).name.is_empty() } {
+                if f.deref().borrow().name.is_empty() {
                     print!("<main>");
                 } else {
-                    print!("<fn {}>", unsafe { f.as_ref().unwrap().name.clone() });
+                    print!("<fn {}>", f.deref().borrow().name);
                 }
             }
             Value::NativeFunction(_) => print!("<native fn>"),
             Value::Closure(c) => {
-                Value::Function(unsafe { (*(*c)).function }).visit()
+                Value::Function(c.deref().borrow().function).visit()
             },
             Value::Upvalue(_) => print!("upvalue"),
             Value::Instance(i) => {
-                print!("{} instance", unsafe { (*i.as_ref().unwrap().class).name.clone() });
+                print!("{} instance", i.deref().borrow().class.deref().borrow().name.clone());
             },
             Value::Method(m) => {
-                Value::Function(unsafe { (*(*(*m)).method).function } ).visit();
+                Value::Function(m.deref().borrow().method.deref().borrow().function).visit();
             },
             _ => panic!("Unknown value!"),
         }
@@ -314,4 +329,9 @@ impl FalseVisitor for Value {
     fn visit_false(&self) -> bool {
         matches!(self, Value::Null)
     }
+}
+
+/// Returns a zero initialized value of type T
+fn zero_init<T>() -> T {
+    unsafe { std::mem::zeroed() }
 }
